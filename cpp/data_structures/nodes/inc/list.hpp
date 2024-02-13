50e8d953efcbb26dd58c4b1d6de69761183c463b
Juan Pablo Castellanos Flores <castellanos.juan.pablo@gmail.com>
2024-02-13 17:10:28 -0600

    Updating complete linked list
    Updating complete linked list and test

--- cpp/data_structures/nodes/inc/list.hpp
+++ cpp/data_structures/nodes/inc/list.hpp
@@ -5,30 +5,51 @@
 
 #include <iostream>
 
+
 template<typename T>
 class Linked_list {
 public:
 	Linked_list(const T &t)
-	: m_size{1} {
+	: m_size{1}
+	{
 		p_tail = p_head = new Data_node<T>(t);
 	}
 
 	Linked_list()
-	: p_head{},p_tail{},m_size{} {
-
+	: p_head{},p_tail{},m_size{}
+	{
 	}
 
 	~Linked_list() {
-		while(p_head != nullptr)
-		{
-			Data_node<T> *temp= p_head;
-			p_head = p_head->next;
-			delete(temp);
-		}
-		p_tail = p_head;
-		m_size = 0;
+		delete_nodes();
 	}
 
+	Linked_list(const Linked_list<T> &r_other_linked_list)
+	: p_head{},p_tail{},m_size{}
+	{
+		auto temp_node =  r_other_linked_list.get_head_node();
+		while(temp_node != nullptr)
+		{
+				add_tail(temp_node->data);
+				temp_node = temp_node->next;
+		}
+	}
+
+	Linked_list<T> & operator=(const Linked_list<T> &r_other_linked_list)
+	{
+		if(this != &r_other_linked_list)
+		{
+			//We need to delete the elements in the current list
+			delete_nodes();
+			auto temp_node =  r_other_linked_list.get_head_node();
+			while(temp_node != nullptr)
+			{
+					add_tail(temp_node->data);
+					temp_node = temp_node->next;
+			}
+		}
+		return *this;
+	}
 
 	size_t size() {
 		return m_size;
@@ -62,18 +83,90 @@ public:
 			return false;
 	}
 
-	std::optional<T> get_head() {
+	std::optional<T> get_head()
+	{
 		if(empty())
 			return std::nullopt;
 		else
 			return std::optional<T> {p_head->data};
 	}
 
-	std::optional<T> get_tail() {
+	std::optional<T> get_tail()
+	{
 		if(empty())
 			return std::nullopt;
 		else
 			return std::optional<T> {p_tail->data};
+	}
+
+	bool delete_node(T item)
+	{
+		//Initialize the temp pointer to the head
+		auto p_temp_node = p_head;
+		//We assume the data is not int the list
+		bool found = false;
+		//If the data is nullptr we don't do anything
+		if(p_temp_node)
+		{
+			//Check if the data is in the head
+			if(p_temp_node->data == item)
+			{
+				//Is in the head
+				p_head=p_head->next;
+				delete(p_temp_node);
+				m_size--;
+				found = true;
+				//If p_head is nullptr, this means the list is empty so we need
+				//to adjust the head
+				if(p_head == nullptr)
+				{
+					p_tail = p_head;
+				}
+			}
+			else
+			{
+				//Set previews the the head
+				auto p_prev_node = p_temp_node;
+				//set the next ptr to the element after the head since the head
+				//case was covered before
+				p_temp_node= p_temp_node->next;
+				while(p_temp_node != nullptr)
+				{
+					//If the temp pointer has the data, link the previous node
+					//to the current next node and delete the current node
+					if(p_temp_node->data == item)
+					{
+						p_prev_node->next=p_temp_node->next;
+						delete(p_temp_node);
+						m_size--;
+						found = true;
+						break;
+					}
+					//Update previous node to the current node
+					p_prev_node = p_temp_node;
+					//Move the current node to the next
+					p_temp_node= p_temp_node->next;
+				}
+			}
+
+		}
+		return found;
+	}
+
+	bool is_in_list(T item)
+	{
+		bool found = false;
+		for(auto note_ptr = p_head;
+			note_ptr != nullptr;
+			note_ptr = note_ptr->next)
+		{
+			if(note_ptr->data == item)
+			{
+				found = true;
+				break;
+			}
+		}
+		return found;
 	}
 
 
@@ -125,7 +218,18 @@ public:
 		return tail_val;
 	}
 
-	void print() {
+	Data_node<T> *get_head_node() const
+	{
+		return static_cast<Data_node<T> * const>(p_head);
+	}
+
+	Data_node<T> *get_tail_node() const
+	{
+		return static_cast<Data_node<T> * const>(p_tail);
+	}
+
+	void print() const
+	{
 		std::cout<<"Printing the list"<<std::endl;
 		for(Data_node<T> *itr = p_head; itr != nullptr; itr=itr->next)
 		{
@@ -135,17 +239,22 @@ public:
 		std::cout<<std::endl;
 	}
 
-	const T* cbegin()
-	{
-		return static_cast<const T*>(p_head);
-	}
-
-
-
 private:
 	Data_node<T> *p_head;
 	Data_node<T> *p_tail;
 	size_t m_size;
+
+	void delete_nodes()
+	{
+		while(p_head != nullptr)
+		{
+			Data_node<T> *temp= p_head;
+			p_head = p_head->next;
+			delete(temp);
+		}
+		p_tail = p_head;
+		m_size = 0;
+	}
 
 };
 
--- tests/data_nodes/node_test.cpp
+++ tests/data_nodes/node_test.cpp
@@ -7,9 +7,147 @@
 #include <type_traits>
 #include <iterator>
 #include <algorithm>
+#include <list>
 
 
-TEST(Linked_list_construction, element_and_empty)
+enum class Llist_insrt_type { head, tail};
+enum class Llist_insrt_mode { reverse, forward};
+
+
+auto g_data_array = std::array<int,4>{1,2,3,4};
+
+//Definition of the array fundamental type
+typedef typename std::remove_pointer<decltype(g_data_array.data())>::type array_t;
+
+//Definition of the array size
+static constexpr std::size_t array_s = std::size(g_data_array);
+
+
+//array iterators
+auto cbegin = g_data_array.cbegin();
+auto cend = g_data_array.cend();
+
+//reverse iterators for the array
+auto rev_cbegin = std::make_reverse_iterator(cend);
+auto rev_cend = std::make_reverse_iterator(cbegin);
+
+
+template <typename T, std::size_t S>
+void llist_init(std::array<T,S> &ref_array,
+                        Linked_list<T> &list,
+                        Llist_insrt_type insrt_type,
+                        Llist_insrt_mode mode)
+{
+    auto cbegin_itr = ref_array.cbegin();
+    auto cend_itr = ref_array.cend();
+
+    auto insert_lambda = [&](auto element)
+    {
+        if(insrt_type == Llist_insrt_type::head)
+        {
+            list.add_head(element);
+        }
+        else
+        {
+           list.add_tail(element); 
+        }
+    };
+
+    if(mode == Llist_insrt_mode::reverse)
+    {
+        auto rev_cbegin_itr = std::make_reverse_iterator(cend_itr);
+        auto rev_cend_itr = std::make_reverse_iterator(cbegin_itr);
+        std::for_each(rev_cbegin_itr,rev_cend_itr,insert_lambda);
+    }
+    else
+    {
+        std::for_each(cbegin_itr,cend_itr,insert_lambda);
+    }
+
+}
+
+
+template <typename T, std::size_t S>
+void comp_and_del_llist_vs_array(   std::array<T,S> &ref_array,
+                                    Linked_list<T> &list,
+                                    Llist_insrt_type insrt_type,
+                                    Llist_insrt_mode mode)
+{
+    //array iterators
+    auto cbegin_itr = ref_array.cbegin();
+    auto cend_itr = ref_array.cend();
+
+
+    auto compare_lambda = [&] (auto element)
+    {
+        std::optional<T> val;
+        if(insrt_type == Llist_insrt_type::head)
+        {
+            val = list.delete_head();
+        }
+        else
+        {
+           val = list.delete_tail();
+        }
+        EXPECT_EQ(val.value(),element);
+    };
+
+    if(mode == Llist_insrt_mode::reverse)
+    {
+        auto rev_cbegin_itr = std::make_reverse_iterator(cbegin_itr);
+        auto rev_cend_itr = std::make_reverse_iterator(cend_itr);
+        std::for_each(rev_cbegin_itr,rev_cend_itr,compare_lambda);
+    }
+    else
+    {
+        std::for_each(cbegin_itr,cend_itr,compare_lambda);
+    }
+
+}
+
+template <typename T>
+void comp_std_list_vs_llist(std::list<T> &std_list,Linked_list<T> &list)
+{
+    std::for_each(  std_list.begin(),
+                    std_list.end(),
+                    [&](auto element)
+                    {
+                        auto list_element = list.delete_head();
+                        EXPECT_EQ(element,list_element.value());
+                    }
+                );
+}
+
+
+void find_remove_test(size_t remove_postition)
+{
+    //Linked list used for testing
+    Linked_list<array_t> l_list {};
+    //an std list for testing
+    std::list<array_t> t_list (g_data_array.cbegin(),g_data_array.cend());
+    //add elements to the tail
+    llist_init<array_t,array_s>(    g_data_array,
+                                    l_list,
+                                    Llist_insrt_type::tail,
+                                    Llist_insrt_mode::forward );
+    //Look for the tail
+    EXPECT_EQ(l_list.is_in_list(g_data_array.at(remove_postition)),true);
+    //Try to delete from the tail
+    EXPECT_EQ(l_list.delete_node(g_data_array.at(remove_postition)),true);
+    //Delete from he tail in the std list
+    t_list.remove(g_data_array.at(remove_postition));
+    //Look for tail it should not be there
+    EXPECT_EQ(l_list.is_in_list(g_data_array.at(remove_postition)),false);
+    //check the std list is not in the std list
+    auto test = std::find(  t_list.cbegin(),
+                            t_list.cend(),
+                            g_data_array.at(remove_postition));
+    //Check the that the element is not there
+    EXPECT_EQ(test,t_list.cend());   
+}
+
+
+TEST(Linked_list, element_and_empty)
 {
     auto data = "hola";
 	auto single_list  = Linked_list<decltype(data)> {data};
@@ -18,86 +156,151 @@ TEST(Linked_list_construction, element_and_empty)
     EXPECT_EQ(empty_list.size(), 0);
 }
 
-TEST(Linked_list_construction, addition_head_tail_checks)
+
+TEST(Linked_list, addition_head_tail_checks)
 {
-    auto data_array = std::array<int,4>{1,2,3,4};
-    auto l_list  = Linked_list<std::remove_pointer<decltype(data_array.data())>::type> {};
 
-    //array iterators
-    auto cbegin = data_array.cbegin();
-    auto cend = data_array.cend();
-
-    //reverse iterators for the array
-    auto rev_cbegin = std::make_reverse_iterator(cend);
-    auto rev_cend = std::make_reverse_iterator(cbegin);
-
-    //Lambda to add elements to the tail
-    auto insert_tail = [&](auto element)
-    {
-        l_list.add_tail(element);
-    };
-
-    //Lambda to add elements to the head
-    auto insert_head = [&](auto element)
-    {
-        l_list.add_head(element);
-    };
+    //Linked list used for testing
+    Linked_list<array_t> l_list {};
 
     //We need to check that an empty list returns an empty optional
     EXPECT_EQ(bool{l_list.get_head()},false);
     EXPECT_EQ(bool{l_list.get_tail()},false);
 
     //add elements to the tail
-    std::for_each(cbegin,cend,insert_tail);
+    llist_init<array_t,array_s>(    g_data_array,
+                                    l_list,
+                                    Llist_insrt_type::tail,
+                                    Llist_insrt_mode::forward );
 
     //Check the same amount of data was added
-    EXPECT_EQ(l_list.size(),data_array.size());
+    EXPECT_EQ(l_list.size(),g_data_array.size());
 
 
     //check that the head has the right value
-    EXPECT_EQ(l_list.get_head().value(),*(cbegin));
+    EXPECT_EQ(l_list.get_head().value(),g_data_array.at(0));
 
     //check that the tail have the right value
-    EXPECT_EQ(l_list.get_tail().value(),*(rev_cbegin));
+    EXPECT_EQ(l_list.get_tail().value(),g_data_array.at(array_s-1));
 
     //check the list is not empty
     EXPECT_EQ(l_list.empty(),false);
 
-    auto check_head_remove = [&] (auto element) {
-        auto val = l_list.delete_head();
-        EXPECT_EQ(val.value(),element);
-    };
-
-    //Remove all elements form the head and make sure they match
-    //the array
-    std::for_each(cbegin,cend,check_head_remove);
+    //Remove all elements form the head and compare against the array
+    comp_and_del_llist_vs_array<array_t,array_s>(   g_data_array,
+                                                    l_list,
+                                                    Llist_insrt_type::head,
+                                                    Llist_insrt_mode::forward );
 
     //Make sure the size list is 0 and Empty
     EXPECT_EQ(l_list.size(),0);
     EXPECT_EQ(l_list.empty(),true);
 
-    //Insert head test
-    std::for_each(rev_cbegin,rev_cend,insert_head);
+    //Inser form head in reverse mode
+    llist_init<array_t,array_s>(    g_data_array,
+                                    l_list,
+                                    Llist_insrt_type::head,
+                                    Llist_insrt_mode::reverse );
 
-    //Make sure the insert head matches the array
-    std::for_each(cbegin,cend,check_head_remove);
+    //Make sure the reversed insert head matches the array
+    comp_and_del_llist_vs_array<array_t,array_s>(   g_data_array,
+                                                    l_list,
+                                                    Llist_insrt_type::head,
+                                                    Llist_insrt_mode::forward );
 
     //Insert from head for testing the tail remove
-
-    //Insert head test
-    std::for_each(cbegin,cend,insert_head);
+    llist_init<array_t,array_s>(    g_data_array,
+                                    l_list,
+                                    Llist_insrt_type::head,
+                                    Llist_insrt_mode::forward );
 
     //Make sure the insert head matches the array
-    std::for_each(cbegin,cend,
-        [&] (auto element)
-        {
-            auto val = l_list.delete_tail();
-            EXPECT_EQ(val.value(),element);
-        }
-    );
+    comp_and_del_llist_vs_array<array_t,array_s>(   g_data_array,
+                                                    l_list,
+                                                    Llist_insrt_type::tail,
+                                                    Llist_insrt_mode::forward );
+}
+
+TEST(Linked_list, copy_constructor_copy_assigment_test)
+{
+    //Linked list used for testing
+    Linked_list<array_t> l_list {};
+
+    //add elements to the tail
+    llist_init<array_t,array_s>(    g_data_array,
+                                    l_list,
+                                    Llist_insrt_type::tail,
+                                    Llist_insrt_mode::forward );
+
+
+    //Copy construct to elements
+    Linked_list<array_t> l_list_copy{l_list};
+    Linked_list<array_t> l_list_copy_assigned = l_list;
+
+
+    //Remove all items form the first list after the copy construction
+    comp_and_del_llist_vs_array<array_t,array_s>(   g_data_array,
+                                                    l_list,
+                                                    Llist_insrt_type::head,
+                                                    Llist_insrt_mode::forward );
+
+    //Make sure the copy constructed Items have the array size
+    EXPECT_EQ(l_list_copy.size(),array_s);
+    EXPECT_EQ(l_list_copy_assigned.size(),array_s);
+
+    //Create a new array of one item bigger than the global size
+    std::array<array_t,array_s+1> array_test {};
+    std::copy(g_data_array.cbegin(),g_data_array.cend(),array_test.begin());
+    //Populate the extra element added
+    array_test[array_s]=array_s;
+
+    //Create a new list with default constructor
+    Linked_list<array_t> secod_array_test {};
+
+    //Populate the list based on the new array
+    llist_init<array_t,array_s+1>(    array_test,
+                                    secod_array_test,
+                                    Llist_insrt_type::tail,
+                                    Llist_insrt_mode::forward );
+
+    //Make sure the linked list has the proper size
+    EXPECT_EQ(secod_array_test.size(),array_s+1);
+
+    //Copy assign the new list to one of the copy constructed list
+    secod_array_test = l_list_copy_assigned;
+
+    EXPECT_EQ(secod_array_test.size(),array_s);
+
+    //Make sure the secod_array_test matches the data after the copy assignment
+    comp_and_del_llist_vs_array<array_t,array_s>(   g_data_array,
+                                                    secod_array_test,
+                                                    Llist_insrt_type::head,
+                                                    Llist_insrt_mode::forward );
+    //Check new sizes
+    EXPECT_EQ(secod_array_test.size(),0);
+    EXPECT_EQ(l_list_copy_assigned.size(),array_s);
 
 }
 
+TEST(Linked_list, find_delete_head_mid_tail)
+{
+    //Remove form head
+    find_remove_test(0);
+    //Remove form middle
+    find_remove_test(array_s/2);
+    //Remove form tail
+    find_remove_test(array_s-1);
+}
+
+TEST(Linked_list, find_delete_empty)
+{
+    //Linked list used for testing
+    Linked_list<array_t> l_list {};
+    //Look for head
+    EXPECT_EQ(l_list.is_in_list(g_data_array.at(0)),false);
+    //Try to delete form an empty list
+    EXPECT_EQ(l_list.delete_node(g_data_array.at(0)),false);
+}
 
 
 int main(int nArgs, char** vArgs) {
